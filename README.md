11f02459f9fb9af728cba9799f0d65ac2bc63f5c
mouse <hardwork.mouse@gmail.com>
2016-01-18 13:53:16 +0200

    V 0.0.1

--- README.md
+++ README.md
@@ -4,5 +4,55 @@ Sometimes we all need to have a some VC capability for the records in DB.
 For example - a history of the profile changes or the article editing...
 Sometimes we need to have the unprovided changes (visible to the author only) etc.
 
-Here is my simple solution for using with any existing model. 
-All you need is add the `trait` to your model.
+Here is my simple solution for using with any existing model.
+ 
+All you need is add the `trait` to ActiveQuery class for your model
+and define VC settings in `$versionControl`.
+
+~~~php
+use \iuriip\yii2-versions\VersionControl;
+
+    /**
+     * @var array $versionControl
+     */
+    public $versionControl = [
+        // Grouping field name.
+        // VC is disabled if empty
+        'branch' => false,
+        // Version ID field name. 
+        // PK used if empty
+        'version' => false,
+        // Provided marker field name. 
+        // VC not using the "with providing" capability if empty
+        'provide' => false,
+        ];
+~~~
+
+### Meanings
+
+* `branch` is a field name for identify the object with versioning. 
+I.e. `user_id` for the user's profiles history, 
+`acticle_key` for the article's edit history etc.
+
+* `version` is a field name for identify the versions.
+You may omit it and use the more fast versioning over PK (it is Unique and Ascending).
+But sometime you need mark the versions by a timestamp or any other ascending value.
+You can define own method for filling this field.
+
+* `provide` is a field name or any possible parameter (acceptable for `where`)
+for marking the version as **provided**. 
+Just define it if you need use this capability.
+Once defined a parameter will used for excluding an **unprovided** last record. 
+But an unprovided record will be accessed if the `$forEdit` parameter of 
+the `version()` method will be `true`.
+
+### Using
+
+You can use the 'version()' method for selecting the last versions 
+anywhere you need.
+
+~~~php
+    public function search($params)
+    {
+        $query = Versioned::find()->version(true);
+~~~
--- VersionControl.php
+++ VersionControl.php
@@ -3,39 +3,45 @@
 use \yii\db\ActiveQuery;
 
 trait VersionControl {
+
     /**
-     * @var string $vcBranch grouping field name. 
-     * VC is disabled if empty
+     * @var array $versionControl
      */
-//    public $vcBranch = false;
-    /**
-     * @var string $vcVersion versions ID field name. 
-     * PK used if empty
-     */
-//    public $vcVersion = false;
-    /**
-     * @var string $vcProvided provided marker field name. 
-     * VC not using the "with providing" capability if empty
-     */
-//    public $vcProviding = false;
+    public $versionControl = [
+        // Grouping field name.
+        // VC is disabled if empty
+        'branch' => false,
+        // Version ID field name. 
+        // PK used if empty
+        'version' => false,
+        // Provided marker for using in `where`. 
+        // VC not using the "with providing" capability if empty
+        'provide' => false,
+    ];
 
     /**
      * Get last version.
      * 
-     * @param boolean $forEdit true if last unprovided record must be visible
-     * i.e. author or moderator need access it
+     * @param boolean $forEdit true if last unprovided record must be visible.
+     * I.e. author or moderator need access it
      */
     public function version($forEdit = false) {
-        if (!empty($this->vcBranch)) {
+        if (!empty($this->versionControl['branch'])) {
             /* @var $class string */
             $class = $this->modelClass;
             /* @var $vcVersion string */
-            $vcVersion = empty($this->vcVersion) ?
-                    $class::primaryKey() : "CONCAT({$this->vcBranch},{$this->vcVersion})";
+            $vcVersion = empty($this->versionControl['version']) ?
+                    // PK is unique
+                    $class::primaryKey()
+                    // Need unique index
+                    : "CONCAT({$this->versionControl['branch']},{$this->versionControl['version']})";
             /* @var $subQuery \yii\db\ActiveQuery */
-            $subQuery = (new ActiveQuery($class))->select(["MAX({$vcVersion})"])->groupBy($this->vcBranch);
-            if (!empty($this->vcProviding) && !$forEdit) {
-                $subQuery = $subQuery->where($this->vcProviding);
+            $subQuery = (new ActiveQuery($class))
+                    ->select(["MAX({$vcVersion})"])
+                    ->groupBy($this->versionControl['branch']);
+            if (!empty($this->versionControl['provide']) && !$forEdit) {
+                $subQuery = $subQuery
+                        ->where($this->versionControl['provide']);
             }
             return $this->andWhere(['in', $vcVersion, $subQuery]);
         }
